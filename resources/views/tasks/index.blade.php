c5f2dc49734eb0def099868130e7bf7d5e7c94d3
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-11-23 08:04:24 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,6 +10,7 @@
                 <tr>
                     <th>id</th>
                     <th>タスク</th>
+                    <th>ステータス</th>
                 </tr>
             </thead>
             <tbody>
@@ -17,6 +18,7 @@
                 <tr>
                     {{-- メッセージ詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
