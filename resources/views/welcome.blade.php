bc5f384e63c069a76d911cc9ffdf82e041660f9c
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-11-28 04:57:40 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Tasklist</h1>
+            <h1>Welcome to the TaskList</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
