336b2ee77a532fc0306d05ff93f59511ba1bdb1c
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-11-28 05:27:39 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,17 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('tasks', 'TasksController');
+/*Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');*/
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 // 認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
