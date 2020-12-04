4cd65b10788c198c446ca8edd921d99d3de1cf76
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-12-04 12:05:45 +0000

    post sixth

--- database/migrations/2020_11_28_150239_add_user_id_to_tasks_table.php
+++ database/migrations/2020_11_28_150239_add_user_id_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddUserIdToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('user_id');
+            $table->string('user_id')->unsigned();
             
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
