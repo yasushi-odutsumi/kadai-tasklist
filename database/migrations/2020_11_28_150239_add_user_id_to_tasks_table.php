96dac82921afcfe9e8dec45b12b40c37c5a993b8
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-12-05 01:45:48 +0000

    post seventh

--- database/migrations/2020_11_28_150239_add_user_id_to_tasks_table.php
+++ database/migrations/2020_11_28_150239_add_user_id_to_tasks_table.php
@@ -14,7 +14,8 @@ class AddUserIdToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('user_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id');
             
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
@@ -29,7 +30,6 @@ class AddUserIdToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('user_id');
             $table->dropForeign(['user_id']);
         });
     }
