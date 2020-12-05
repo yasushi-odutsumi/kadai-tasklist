3e85c7bafc6adc66de6d1b1687a2c304e8155bd8
yasushi.odutsumi <yasuozu0122@icloud.com>
2020-12-05 07:35:02 +0000

    tasks index fix

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,6 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>user_id</th>
                     <th>タスク</th>
                     <th>ステータス</th>
                 </tr>
@@ -19,7 +18,6 @@
                 <tr>
                     {{-- メッセージ詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->user_id }}</td>
                     <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
